d7b70dbc6809426c94bdde676c942a84d2513e05
David Buzinski <davidb@mathworks.com>
2023-09-29 14:31:33 -0400

    why no work

--- src/post.ts
+++ src/post.ts
@@ -3,14 +3,9 @@
 import * as core from "@actions/core";
 import { cacheMATLAB } from "./cache-save";
 
-// Instead of failing this action, just log and warn.
-process.on('uncaughtException', e => {
-    const warningPrefix = '[warning]';
-    core.info(`${warningPrefix}${e.message}`);
-});
-
 export async function run() {
     const useCache = core.getInput('use-cache');
+    core.info(useCache);
     await cacheMATLAB(useCache);
 }
 
